1fd9104eac73e0c52dfc1073393c9ef7d04cc6d1
Oskar Adin <oskar@copygr.am>
2013-11-07 19:29:23 +0100

    Checking for bad db-user in config

--- wpversion.sh
+++ wpversion.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 clear
 
-#===Color Section====#
+# Columns
+FIRST=50
+SECOND=20
+
+# Is BADUSER used for the DB?
+BADUSER="root"
+
+# Colors
 BLACK='\e[30;49m';
 RED='\e[31;49m';
 GREEN='\e[32;49m';
@@ -10,17 +17,16 @@ BLUE='\e[34;49m';
 MAGENTA='\e[35;49m';
 CYAN='\e[36;49m';
 WHITE='\e[37;49m';
-#Reset colors.
 RESET="\e[0m";
 
 # Path to folder where we can find WP installs
 CHECKDIR=""
 
-DIRS=`ls -l $CHECKDIR | egrep '^d' | awk '{print $9}'`
-WPVERSION=`curl -L -s http://wordpress.org/download/ | grep -E -o 'Version ([0-9]\.[0-9])(\.[0-9])?' | sed 's/Version //'`
+DIRS=$(ls -l $CHECKDIR | egrep '^d' | awk '{print $9}')
+WPVERSION=$(curl -L -s http://wordpress.org/download/ | grep -E -o 'Version ([0-9]\.[0-9])(\.[0-9])?' | sed 's/Version //')
 
 # Print headers
-printf "$BLUE%-40s Version (%s)$RESET\n" "Folder"  $WPVERSION
+printf "$BLUE%-${FIRST}s %-${SECOND}s %s$RESET\n" "Folder" "Version ($WPVERSION)" "DB-USER"
 printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
 
 # Print customer and version
@@ -28,10 +34,12 @@ for DIR in $DIRS
 do
     FULLPATH="$CHECKDIR/$DIR"
 
-    if [ -e "$FULLPATH/wp-includes/version.php" ]
-    then
+    # If a WP-folder
+    if [ -e "$FULLPATH/wp-includes/version.php" ]; then
         VERSIONFILE="$FULLPATH/wp-includes/version.php"
-        CUSTOMERVERSION=`grep '$wp_version =' ${VERSIONFILE} | awk '{print $3}' | sed 's/'\''\|;//g'`
+        CUSTOMERVERSION=$(grep '$wp_version =' ${VERSIONFILE} | awk '{print $3}' | sed 's/'\''\|;//g')
+
+        DBUSER=$(grep  -oP "DB_USER', '\K([a-zA-Z]*)" "${FULLPATH}/wp-config.php")
 
         # Replace dots and pad right with spaces.
         # Then we replace spaces with 0
@@ -41,11 +49,18 @@ do
         WV=${WV// /0}
 
         if (( CV < WV )); then
-            TEXTCOLOR=$RED
+            VERSIONCOLOR=$RED
         else
-            TEXTCOLOR=$GREEN
+            VERSIONCOLOR=$GREEN
         fi
 
-        printf "%-40s $TEXTCOLOR%s$RESET\n" ${DIR} ${CUSTOMERVERSION}
+        # Check for BADUSER
+        shopt -s nocasematch
+        case "$DBUSER" in
+            $BADUSER ) USERCOLOR=$RED;;
+            *) USERCOLOR=$GREEN;;
+        esac
+
+        printf "%-${FIRST}s $VERSIONCOLOR%-${SECOND}s$RESET $USERCOLOR%s$RESET\n" ${DIR} ${CUSTOMERVERSION} ${DBUSER}
     fi
 done

ea6583238050735b1f05257cbe96e3d41d57f25c
Oskar Adin <oskar@copygr.am>
2013-11-08 07:51:48 +0100

    Removed unnecessary sed with grep caption groups

--- wpversion.sh
+++ wpversion.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 clear
 
+# Path to folder where we can find WP installs
+CHECK_DIR=""
+
 # Columns
 FIRST=50
 SECOND=20
 
 # Is BADUSER used for the DB?
-BADUSER="root"
+BAD_USER="root"
 
 # Colors
 BLACK='\e[30;49m';
@@ -19,48 +22,48 @@ CYAN='\e[36;49m';
 WHITE='\e[37;49m';
 RESET="\e[0m";
 
-# Path to folder where we can find WP installs
-CHECKDIR=""
+VERSION_REGEX="(([0-9]\.[0-9])(\.[0-9])?)"
 
-DIRS=$(ls -l $CHECKDIR | egrep '^d' | awk '{print $9}')
-WPVERSION=$(curl -L -s http://wordpress.org/download/ | grep -E -o 'Version ([0-9]\.[0-9])(\.[0-9])?' | sed 's/Version //')
+DIRS=$(ls -l $CHECK_DIR | egrep '^d' | awk '{print $9}')
+WP_VERSION=$(curl -L -s http://wordpress.org/download/ | grep -oP "Version \K(${VERSION_REGEX})")
 
-# Print headers
-printf "$BLUE%-${FIRST}s %-${SECOND}s %s$RESET\n" "Folder" "Version ($WPVERSION)" "DB-USER"
+# Print head
+printf "$BLUE%-${FIRST}s %-${SECOND}s %s$RESET\n" "Folder" "Version ($WP_VERSION)" "DB-USER"
 printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
 
 # Print customer and version
 for DIR in $DIRS
 do
-    FULLPATH="$CHECKDIR/$DIR"
+    FULL_PATH="$CHECK_DIR/$DIR"
+    WP_VERSION_FILE="$FULL_PATH/wp-includes/version.php"
+    WP_CONFIG_FILE="$FULL_PATH/wp-config.php"
 
-    # If a WP-folder
-    if [ -e "$FULLPATH/wp-includes/version.php" ]; then
-        VERSIONFILE="$FULLPATH/wp-includes/version.php"
-        CUSTOMERVERSION=$(grep '$wp_version =' ${VERSIONFILE} | awk '{print $3}' | sed 's/'\''\|;//g')
-
-        DBUSER=$(grep  -oP "DB_USER', '\K([a-zA-Z]*)" "${FULLPATH}/wp-config.php")
+    # WP-folder?
+    if [ -e "$WP_VERSION_FILE" ]; then
+        CUSTOMER_WP_VERSION=$(grep -oP "wp_version = '\K(${VERSION_REGEX})" "${WP_VERSION_FILE}")
+        DB_USER=$(grep -oP "DB_USER', '\K([a-zA-Z]*)" "${WP_CONFIG_FILE}")
 
         # Replace dots and pad right with spaces.
-        # Then we replace spaces with 0
-        CV=$(printf "%-03s" ${CUSTOMERVERSION//.})
+        # Then replace spaces with 0
+        CV=$(printf "%-03s" ${CUSTOMER_WP_VERSION//.})
         CV=${CV// /0}
-        WV=$(printf "%-03s" ${WPVERSION//.})
+        WV=$(printf "%-03s" ${WP_VERSION//.})
         WV=${WV// /0}
 
+        # CUSTOMER_VERSION -lt WORDPRESS_VERSION?
         if (( CV < WV )); then
-            VERSIONCOLOR=$RED
+            WP_VERSION_COLOR=$RED
         else
-            VERSIONCOLOR=$GREEN
+            WP_VERSION_COLOR=$GREEN
         fi
 
-        # Check for BADUSER
+        # Check for BAD_USER
         shopt -s nocasematch
-        case "$DBUSER" in
-            $BADUSER ) USERCOLOR=$RED;;
-            *) USERCOLOR=$GREEN;;
+        case "$DB_USER" in
+            $BAD_USER ) USER_COLOR=$RED;;
+            *) USER_COLOR=$GREEN;;
         esac
 
-        printf "%-${FIRST}s $VERSIONCOLOR%-${SECOND}s$RESET $USERCOLOR%s$RESET\n" ${DIR} ${CUSTOMERVERSION} ${DBUSER}
+        printf "%-${FIRST}s $WP_VERSION_COLOR%-${SECOND}s$RESET $USER_COLOR%s$RESET\n" ${DIR} ${CUSTOMER_WP_VERSION} ${DB_USER}
     fi
 done
